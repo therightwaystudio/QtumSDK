828d4553a746c120f3194a09b4af5fd09fea63ba
Maxim <therightwaystudio@gmail.com>
2017-04-27 11:23:02 +0300

    add sample

---
+++ app/src/main/java/org/qtum/mromanovsky/sampleqtumsdk/MainActivity.java
@@ -0,0 +1,19 @@
+package org.qtum.mromanovsky.sampleqtumsdk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.qtum.mromanovsky.qtumsdk.QtumPaymentActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, QtumPaymentActivity.class);
+        startActivity(intent);
+    }
+}
